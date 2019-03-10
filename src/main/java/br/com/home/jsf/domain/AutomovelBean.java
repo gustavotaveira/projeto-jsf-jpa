703079ac67593f87aef68043df0b8f5a611a1b4c
Gustavo Vinicius Taveira Lima <gustavo_vinicius_taveira@hotmail.com>
2019-03-10 19:32:07 -0300

    Adicionando Listagem de automoveis via datatable

--- src/main/java/br/com/home/jsf/domain/AutomovelBean.java
+++ src/main/java/br/com/home/jsf/domain/AutomovelBean.java
@@ -1,11 +1,18 @@
 package br.com.home.jsf.domain;
 
+import br.com.home.jsf.infra.JPAUtil;
+
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean
 public class AutomovelBean {
 
   private Automovel automovel = new Automovel();
+  private List<Automovel> automoveis;
 
     public Automovel getAutomovel() {
         return automovel;
@@ -16,6 +23,24 @@ public class AutomovelBean {
     }
 
     public void salva(){
-        System.out.println("Salvando automovel: "+automovel);
+        EntityManager entityManager = JPAUtil.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(automovel);
+        entityManager.getTransaction().commit();
+    }
+
+    public List<Automovel> getAutomoveis(){
+        EntityManager entityManager = JPAUtil.getEntityManager();
+        Query q = entityManager.createQuery("select a from Automovel a", Automovel.class);
+        this.automoveis = converteParaListaAutomoveis(q.getResultList());
+        return this.automoveis;
+    }
+
+    private List<Automovel> converteParaListaAutomoveis(List resultList) {
+        List<Automovel> autos = new ArrayList<>();
+        for (Object auto : resultList){
+            autos.add((Automovel) auto);
+        }
+        return autos;
     }
 }
